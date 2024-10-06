f01887379e499b9e728380836fda232d479c7462
Yamei Song <ymsongx@163.com>
2024-10-06 15:33:48 +1100

    Add accelerate>=0.26.0 as a dependency in setup.py

--- examples/NLU/setup.py
+++ examples/NLU/setup.py
@@ -83,6 +83,7 @@ if stale_egg_info.exists():
 # 1. all dependencies should be listed here with their version requirements if any
 # 2. once modified, run: `make deps_table_update` to update src/transformers/dependency_versions_table.py
 _deps = [
+    "accelerate>=0.26.0",
     "black>=20.8b1",
     "cookiecutter==1.7.2",
     "dataclasses",
