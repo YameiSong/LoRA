ca567005eb9c048e85e3e8fad137d424bb29b39b
Yamei Song <ymsongx@163.com>
2024-10-07 17:46:34 +1100

    load_metric is deprecated. Use 'evaluate.load' instead

--- examples/NLU/examples/text-classification/run_glue.py
+++ examples/NLU/examples/text-classification/run_glue.py
@@ -26,6 +26,7 @@ from typing import Optional
 
 import numpy as np
 from datasets import load_dataset, load_metric
+import evaluate
 
 import transformers
 from transformers import (
@@ -510,9 +511,11 @@ def main():
 
     # Get the metric function
     if data_args.task_name is not None and data_args.metric_file is None:
-        metric = load_metric("glue", data_args.task_name)
+        # metric = load_metric("glue", data_args.task_name)
+        metric = evaluate.load_metric("glue", data_args.task_name)
     else:
-        metric = load_metric(data_args.metric_file, trust_remote_code=True)
+        # metric = load_metric(data_args.metric_file, trust_remote_code=True)
+        metric = evaluate.load_metric(data_args.metric_file)
     # TODO: When datasets metrics include regular accuracy, make an else here and remove special branch from
     # compute_metrics
 
